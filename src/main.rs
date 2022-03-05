641e6a822d0a2a8d435217d46189b1c62af0685c
Bruno Thomas <bruno@barreverte.fr>
2022-03-05 14:42:03 +0100

    feat: add env logger

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+#[macro_use]
+extern crate log;
 use auth_proxy::{run_service, ProxyConfig};
 
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     let (_tx, rx) = tokio::sync::oneshot::channel::<()>();
     let config = ProxyConfig::default();
     let server = run_service(config.clone(), rx).await;
-    println!("Running server on {:?}", config.address);
+    info!("Running auth proxy on {:?} with backend {:?}", config.address, config.back_uri);
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
